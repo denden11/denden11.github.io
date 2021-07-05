1fc25de28239bd749870fa2ff4a2801698cdcffb
denden11 <abeniondennis11@gmail.com>
2021-07-05 15:12:11 +0800

    change the code to generate the qrcode

--- main.js
+++ main.js
@@ -1,26 +1,8 @@
-
-var qrcode = new QRCode("qrcode");
-
-function makeCode () {    
-  var elText = document.getElementById("text");
-  
-  if (!elText.value) {
-    alert("Input a text");
-    elText.focus();
-    return;
-  }
-  
-  qrcode.makeCode(elText.value);
-}
-
-makeCode();
-
-$("#text").
-  on("blur", function () {
-    makeCode();
-  }).
-  on("keydown", function (e) {
-    if (e.keyCode == 13) {
-      makeCode();
-    }
-  });
+var qrcode = new QRCode("test", {
+    text: "http://jindo.dev.naver.com/collie",
+    width: 128,
+    height: 128,
+    colorDark : "#000000",
+    colorLight : "#ffffff",
+    correctLevel : QRCode.CorrectLevel.H
+});
